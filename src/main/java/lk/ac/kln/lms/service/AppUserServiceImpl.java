b3d5801bc2eca3e673eb90eb903b0102514e04a5
Shamika Tissera <nimeshshamika@gmail.com>
2022-10-13 18:32:18 +0530

    Created UserController

--- src/main/java/lk/ac/kln/lms/service/AppUserServiceImpl.java
+++ src/main/java/lk/ac/kln/lms/service/AppUserServiceImpl.java
@@ -24,18 +24,20 @@ public class AppUserServiceImpl implements AppUserService {
     private final RoleRepo roleRepo;
     @Override
     public AppUser saveUser(AppUser user) {
+        log.info("Saving new user {} to the database", user.getName());
         return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        //log.info("Saving new role {} to the database", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
         // TODO: Add necessary validations
-
+        log.info("Adding role {} to user {}", roleName, username);
         AppUser user = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
@@ -43,11 +45,13 @@ public class AppUserServiceImpl implements AppUserService {
 
     @Override
     public AppUser getUser(String username) {
+        log.info("Fetching user {}", username);
         return null;
     }
 
     @Override
     public List<AppUser> getUsers() {
+        log.info("Fetching all users");
         return userRepo.findAll();
     }
 
