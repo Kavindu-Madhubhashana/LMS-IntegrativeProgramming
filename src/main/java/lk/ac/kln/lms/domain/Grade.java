3825c02fafe7b7c3126785103e345f4b889ae6d8
PavithMadushan <pavithmadushan@gmail.com>
2022-10-15 18:38:18 +0530

    Grade java class created

--- src/main/java/lk/ac/kln/lms/domain/Grade.java
+++ src/main/java/lk/ac/kln/lms/domain/Grade.java
@@ -1,5 +1,8 @@
 package lk.ac.kln.lms.domain;
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 import lombok.*;
 
 import javax.persistence.*;
@@ -12,9 +15,13 @@ import java.sql.Date;
 @Getter
 @Setter
 
+<<<<<<< Updated upstream
 //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 public class Grade {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+=======
+public class Grade {
+>>>>>>> Stashed changes
 }
