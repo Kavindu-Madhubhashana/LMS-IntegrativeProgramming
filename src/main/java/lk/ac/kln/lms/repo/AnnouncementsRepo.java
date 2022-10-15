799b55840b22a5d41d72282cb012817db2b580bb
malithii <mkarunarathne997@gmail.com>
2022-10-15 18:15:11 +0530

    changes in announcements repo

--- src/main/java/lk/ac/kln/lms/repo/AnnouncementsRepo.java
+++ src/main/java/lk/ac/kln/lms/repo/AnnouncementsRepo.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 @Repository
 public interface AnnouncementsRepo extends JpaRepository<Announcements,Long> {
     Optional<Announcements> findById(final String id);
+    Iterable<Announcements> findByCourseCode(final Course course);
 }
