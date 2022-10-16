5f40762ab1fc803b2400296de40ef2513e11fa5b
Sithum <sithum.dashantha@gmail.com>
2022-10-16 17:58:34 +0530

    Small changes

--- src/main/java/lk/ac/kln/lms/dto/CreateEvaluationDto.java
+++ src/main/java/lk/ac/kln/lms/dto/CreateEvaluationDto.java
@@ -1,16 +1,10 @@
 package lk.ac.kln.lms.dto;
 
-import lk.ac.kln.lms.domain.AppUser;
-import lk.ac.kln.lms.domain.Course;
 import lk.ac.kln.lms.enums.GradeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 
 @Getter
 @Setter
